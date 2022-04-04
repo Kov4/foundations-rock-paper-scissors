7026ca25ba6646eb8e534a3070dbd141f529520f
Kov <guillaume.koltok@gmail.com>
2022-04-04 15:24:44 +0200

    ajout des fonctions qui calculent le score

--- rpsgame.js
+++ rpsgame.js
@@ -1,5 +1,3 @@
-
-
 function computerPlay(){
     let randresult;
     randresult = Math.floor(Math.random() * 3);
@@ -31,63 +29,156 @@ function caseInsensitive(word){
 function playRound(playerSelection, computerSelection){
     playerSelection = caseInsensitive(playerSelection);
     let resultString;
-
+    console.log(playerSelection)
+    console.log(computerSelection)
     if(playerSelection === computerSelection){
-        resultString = "Nobody won, try again !";
+        resultString = "Nobody won this round, try again !";
     }
     else if((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")){
-        resultString = "You win, congratulations !";
+        resultString = "You win this round, congratulations !";
     }
     else{
-        resultString = "You lose ...";
+        resultString = "You lose this round ...";
     }
+
+    console.log(resultString)
+
+    /*const body = document.querySelector('body');
+    const div = document.createElement('div');
+    div.textContent = resultString;
+    body.appendChild(div);*/
+
+    const resultat=document.querySelector('.resultat');
+    resultat.textContent=resultString;
+
     return resultString;
 }
 
+function rockScore(playerScore, computerScore){
+    
+    let result = playRound('rock', computerPlay());
 
-
-
-/*function game(){
-    let winner;
-    let roundResult;
-    let winningPoint = 0;
-    let computerSelection;
-    let playerSelection;
-    for (let i=0; i<5; i++){
-        
-        computerSelection = computerPlay();
-        playerSelection = prompt("Choisir votre signe entre pierre feuille ciseau :");
-        console.log("computer :", computerSelection);
-        console.log('player: ', playerSelection);
-
-        roundResult = playRound(playerSelection, computerSelection);
-        console.log("for this round:", roundResult);
-
-        if (roundResult === "You win, congratulations !"){
-            winningPoint = winningPoint + 1;
-        }
-        else if (roundResult === "You lose ..."){
-            winningPoint = winningPoint - 1;
-        }
+    if(result === 'You win this round, congratulations !'){
+        playerScore = playerScore + 1;
+    }
+    else if(result === 'You lose this round ...'){
+        computerScore = computerScore + 1;
     }
 
-    if (winningPoint > 0){
-        winner = "Player won";
+    console.log(playerScore, computerScore);
+
+    if(playerScore === 5){
+        console.log('Player won the game:)');
+        playerScore = 0;
+        computerScore = 0;
+        const resultat=document.querySelector('.resultat');
+        resultat.textContent='Player won the game:)';
+    }
+    if(computerScore === 5){
+        console.log(('Computer won the game :/'));
+        playerScore = 0;
+        computerScore = 0;
+        const resultat=document.querySelector('.resultat');
+        resultat.textContent='Computer won the game :/';
     }
 
-    else if (winningPoint === 0){
-        winner = "Nobody won";
+    return [playerScore, computerScore];
+}
+
+function paperScore(playerScore, computerScore){
+
+    let result = playRound('paper', computerPlay());
+
+    if(result === 'You win this round, congratulations !'){
+        playerScore = playerScore + 1;
     }
-    else{
-        winner = "Computer won";
+    else if(result === 'You lose this round ...'){
+        computerScore = computerScore + 1;
     }
 
-    return winner;
-}*/
+    console.log(playerScore, computerScore);
 
-//console.log(game());
+    if(playerScore === 5){
+        console.log('Player won the game:)');
+        playerScore = 0;
+        computerScore = 0;
+        const resultat=document.querySelector('.resultat');
+        resultat.textContent='Player won the game:)';
+    }
+    if(computerScore === 5){
+        console.log(('Computer won the game :/'));
+        playerScore = 0;
+        computerScore = 0;
+        const resultat=document.querySelector('.resultat');
+        resultat.textContent='Computer won the game :/';
+    }
 
-const rockBut = document.querySelector('button');
-const btn = document.querySelector('#btn');
-//rockButton.addEventListener('click', playRound('Rock',computerPlay));
-console.log(btn);
+    return [playerScore, computerScore];
+    
+}
+
+function scissorsScore(playerScore, computerScore){
+    
+    let result = playRound('paper', computerPlay());
+
+    if(result === 'You win this round, congratulations !'){
+        playerScore = playerScore + 1;
+    }
+    else if(result === 'You lose this round ...'){
+        computerScore = computerScore + 1;
+    }
+
+    console.log(playerScore, computerScore);
+
+    if(playerScore === 5){
+        console.log('Player won the game:)');
+        playerScore = 0;
+        computerScore = 0;
+        const resultat=document.querySelector('.resultat');
+        resultat.textContent='Player won the game:)';
+    }
+    if(computerScore === 5){
+        console.log(('Computer won the game :/'));
+        playerScore = 0;
+        computerScore = 0;
+        const resultat=document.querySelector('.resultat');
+        resultat.textContent='Computer won the game :/';
+    }
+
+    return [playerScore, computerScore];
+
+}
+
+function playGame(){
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    document.querySelectorAll('.rockButton').forEach(item => {
+        item.addEventListener('click', function() {[playerScore, computerScore] = rockScore(playerScore, computerScore);}); 
+    });
+
+    document.querySelectorAll('.paperButton').forEach(item => {
+        item.addEventListener('click', function() {[playerScore, computerScore] = paperScore(playerScore, computerScore);}); 
+    });
+
+    document.querySelectorAll('.scissorsButton').forEach(item => {
+        item.addEventListener('click', function() {[playerScore, computerScore] = scissorsScore(playerScore, computerScore);}); 
+    });       
+}
+
+playGame();
+
+/*
+let computerScore = 0;
+
+const rockButton = document.querySelector('.rockButton');
+rockButton.addEventListener('click', function() {playRound('rock', computerPlay());});
+
+rockButton.addEventListener('click', () => {computerScore = computerScore + 1;});
+
+const paperButton = document.querySelector('.paperButton')
+paperButton.addEventListener('click', function() {playRound('paper', computerPlay());});
+
+const scissorsButton = document.querySelector('.scissorsButton')
+scissorsButton.addEventListener('click', function() {playRound('scissors', computerPlay());});*/
